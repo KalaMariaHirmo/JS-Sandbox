16ea35db9be05e3f461b805f7fd31f660220511a
holger420 <hirmo.holger@gmail.com>
2019-12-09 10:52:33 +0200

    esmasp

--- app.js
+++ app.js
@@ -36,10 +36,75 @@ const numbers2 = new Array(6,7,8,9,10);
 numbers2.forEach(number => {
     console.log("Let count: ", number);
 });
-
+//more arrays
 const mixedArray = [22, "banana", true]
 mixedArray.forEach(element => {
     console.log(typeof(element), element);
 });
 
 console.log("Banana is", mixedArray[1].length, "chars long")
+
+console.log("")
+//adds a new element to the end
+mixedArray.push("apple")
+mixedArray.forEach(element=>{
+    console.log("Updated array: ",element)
+});
+
+console.log("")
+//adds an element to the front
+mixedArray.unshift(1000);
+mixedArray.forEach(element=>{
+    console.log("Updated array: ",element)
+});
+
+console.log("")
+//removes last element
+mixedArray.pop();
+//removes first element 
+mixedArray.shift();
+mixedArray.forEach(element=>{
+    console.log("Updated array: ",element)
+});
+
+console.log("")
+//removes an element at a certain position
+mixedArray.splice(1,1);
+mixedArray.forEach(element=>{
+    console.log("Updated array: ",element)
+});
+
+//Object literals
+const person = {
+    firstName: "Joe",
+    lastName: "Mama",
+    age: 25,
+    email: "JoeMama@gmail.com",
+    hobbies: ['music', 'sports'],
+
+    getYearOfBirth: function() {
+        return 2019 - this.age;
+    }
+}
+
+console.log("")
+
+let val;
+val = person;
+val = person.firstName;
+val = person.lastName;
+val = person['lastName']
+val = person.age;
+val = person.hobbies[0];
+val = person.getYearOfBirth();
+console.log(val);
+
+const people = [
+    {name: "John", age: 25},
+    {name: "Boomer", age: 69},
+    {name: "Zoomer", age: 17},
+]
+
+people.forEach(person =>{
+    console.log(person.name, "is", person.age, "years old.")
+});
